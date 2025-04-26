78a0894ff6a40cb5f937551376c59ed56ecd593b
Samuel Teixeira Passamani <159223630+LordFlixXx@users.noreply.github.com>
2025-04-26 03:13:36 -0300

    Update script.js

--- dist/cam/script.js
+++ dist/cam/script.js
@@ -1,10 +1,17 @@
+"use strict";
+
+// Obtém o valor do parâmetro 'id' na URL
 const urlParams = new URLSearchParams(window.location.search);
-const videoId = urlParams.get("id"); // Obtém o valor do parâmetro 'id' na URL
+const videoId = urlParams.get("id");
 
 if (!videoId) {
   console.error("Nenhum ID foi fornecido na URL. Adicione ?id=valor na URL.");
 } else {
-  // Busca os dados do JSON
+  fetchCameraData(videoId);
+}
+
+// Função para buscar os dados da câmera e configurar o player
+function fetchCameraData(videoId) {
   fetch("https://site.my.eu.org/1:/male.json")
     .then((response) => {
       if (!response.ok) {
@@ -13,7 +20,6 @@ if (!videoId) {
       return response.json();
     })
     .then((data) => {
-      // Verifica se broadcasts.items existe no JSON
       if (data && data.broadcasts && Array.isArray(data.broadcasts.items)) {
         const camera = data.broadcasts.items.find((item) => item.id === videoId);
 
@@ -22,131 +28,7 @@ if (!videoId) {
           return;
         }
 
-        // Configura o player com os dados da câmera correspondente
-        const playerInstance = jwplayer("player").setup({
-          controls: true,
-          sharing: true,
-          autostart: false,
-          displaytitle: true,
-          displaydescription: true,
-          abouttext: "Buy me a coffee ☕",
-          aboutlink: "https://xxx.filmes.net.eu.org/",
-
-          skin: {
-            name: "netflix",
-          },
-
-          logo: {
-            file: "https://i.imgur.com/1inVaQD.png",
-            link: "https://xxx.filmes.net.eu.org",
-          },
-
-          captions: {
-            color: "#efcc00",
-            fontSize: 16,
-            backgroundOpacity: 0,
-            edgeStyle: "raised",
-          },
-
-          playlist: [
-            {
-              title: camera.username,
-              description: camera.tags.map((tag) => tag.name).join(", "),
-              image: camera.preview.poster,
-              sources: [
-                {
-                  file: camera.preview.src,
-                  type: "video/m3u8",
-                  label: "Source",
-                },
-              ],
-            },
-          ],
-        });
-
-        playerInstance.on("ready", function () {
-          const buttonId = "download-video-button";
-          const iconPath =
-            "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNCAyNCIgd2lkdGg9IjY0IiBoZWlnaHQ9IjY0Ij48cGF0aCBmaWxsPSJub25lIiBkPSJNMCAwaDI0djI0SDB6Ii8+P[...]";
-          const tooltipText = "Download Video";
-
-          // Adiciona o botão de download personalizado
-          playerInstance.addButton(
-            iconPath,
-            tooltipText,
-            buttonClickAction,
-            buttonId
-          );
-
-          function buttonClickAction() {
-            const playlistItem = playerInstance.getPlaylistItem();
-            const anchor = document.createElement("a");
-            const fileUrl = playlistItem.file;
-            anchor.setAttribute("href", fileUrl);
-            const downloadName = playlistItem.file.split("/").pop();
-            anchor.setAttribute("download", downloadName);
-            anchor.style.display = "none";
-            document.body.appendChild(anchor);
-            anchor.click();
-            document.body.removeChild(anchor);
-          }
-
-          // Move o timeslider para alinhar com outros controles
-          const playerContainer = playerInstance.getContainer();
-          const buttonContainer = playerContainer.querySelector(
-            ".jw-button-container"
-          );
-          const spacer = buttonContainer.querySelector(".jw-spacer");
-          const timeSlider = playerContainer.querySelector(".jw-slider-time");
-          buttonContainer.replaceChild(timeSlider, spacer);
-
-          // Detecta bloqueadores de anúncios
-          playerInstance.on("adBlock", () => {
-            const modal = document.querySelector("div.modal");
-            modal.style.display = "flex";
-
-            document
-              .getElementById("close")
-              .addEventListener("click", () => location.reload());
-          });
-
-          // Adiciona botão de avançar 10 segundos
-          const rewindContainer = playerContainer.querySelector(
-            ".jw-display-icon-rewind"
-          );
-          const forwardContainer = rewindContainer.cloneNode(true);
-          const forwardDisplayButton = forwardContainer.querySelector(
-            ".jw-icon-rewind"
-          );
-          forwardDisplayButton.style.transform = "scaleX(-1)";
-          forwardDisplayButton.ariaLabel = "Forward 10 Seconds";
-          const nextContainer = playerContainer.querySelector(
-            ".jw-display-icon-next"
-          );
-          nextContainer.parentNode.insertBefore(forwardContainer, nextContainer);
-
-          // Ícone da barra de controle
-          playerContainer.querySelector(
-            ".jw-display-icon-next"
-          ).style.display = "none"; // Oculta botão "Next"
-          const rewindControlBarButton = buttonContainer.querySelector(
-            ".jw-icon-rewind"
-          );
-          const forwardControlBarButton = rewindControlBarButton.cloneNode(true);
-          forwardControlBarButton.style.transform = "scaleX(-1)";
-          forwardControlBarButton.ariaLabel = "Forward 10 Seconds";
-          rewindControlBarButton.parentNode.insertBefore(
-            forwardControlBarButton,
-            rewindControlBarButton.nextElementSibling
-          );
-
-          // Adiciona eventos de clique para avançar 10 segundos
-          [forwardDisplayButton, forwardControlBarButton].forEach((button) => {
-            button.onclick = () => {
-              playerInstance.seek(playerInstance.getPosition() + 10);
-            };
-          });
-        });
+        setupPlayer(camera);
       } else {
         console.error("Estrutura do JSON inválida ou items não encontrados.");
       }
@@ -154,4 +36,124 @@ if (!videoId) {
     .catch((error) =>
       console.error("Erro ao carregar o arquivo JSON:", error)
     );
+}
+
+// Função para configurar o player
+function setupPlayer(camera) {
+  const playerInstance = jwplayer("player").setup({
+    controls: true,
+    sharing: true,
+    autostart: false,
+    displaytitle: true,
+    displaydescription: true,
+    abouttext: "Buy me a coffee ☕",
+    aboutlink: "https://xxx.filmes.net.eu.org/",
+    skin: { name: "netflix" },
+    logo: {
+      file: "https://i.imgur.com/1inVaQD.png",
+      link: "https://xxx.filmes.net.eu.org",
+    },
+    captions: {
+      color: "#efcc00",
+      fontSize: 16,
+      backgroundOpacity: 0,
+      edgeStyle: "raised",
+    },
+    playlist: [
+      {
+        title: camera.username,
+        description: camera.tags.map((tag) => tag.name).join(", "),
+        image: camera.preview.poster,
+        sources: [
+          {
+            file: camera.preview.src,
+            type: "video/m3u8",
+            label: "Source",
+          },
+        ],
+      },
+    ],
+  });
+
+  playerInstance.on("ready", () => {
+    addDownloadButton(playerInstance);
+    alignTimeSlider(playerInstance);
+    handleAdBlockDetection(playerInstance);
+    addForwardButton(playerInstance);
+  });
+}
+
+// Função para adicionar botão de download
+function addDownloadButton(playerInstance) {
+  const buttonId = "download-video-button";
+  const iconPath =
+    "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNCAyNCIgd2lkdGg9IjY0IiBoZWlnaHQ9IjY0Ij48cGF0aCBmaWxsPSJub25lIiBkPSJNMCAwaDI0djI0SDB6Ii8+P[...]";
+  const tooltipText = "Download Video";
+
+  playerInstance.addButton(iconPath, tooltipText, () => {
+    const playlistItem = playerInstance.getPlaylistItem();
+    const anchor = document.createElement("a");
+    anchor.setAttribute("href", playlistItem.file);
+    anchor.setAttribute("download", playlistItem.file.split("/").pop());
+    anchor.style.display = "none";
+    document.body.appendChild(anchor);
+    anchor.click();
+    document.body.removeChild(anchor);
+  }, buttonId);
+}
+
+// Função para alinhar o time slider com outros controles
+function alignTimeSlider(playerInstance) {
+  const playerContainer = playerInstance.getContainer();
+  const buttonContainer = playerContainer.querySelector(".jw-button-container");
+  const spacer = buttonContainer.querySelector(".jw-spacer");
+  const timeSlider = playerContainer.querySelector(".jw-slider-time");
+  buttonContainer.replaceChild(timeSlider, spacer);
+}
+
+// Função para detectar bloqueadores de anúncios
+function handleAdBlockDetection(playerInstance) {
+  playerInstance.on("adBlock", () => {
+    const modal = document.querySelector("div.modal");
+    modal.style.display = "flex";
+
+    document.getElementById("close").addEventListener("click", () => {
+      location.reload();
+    });
+  });
+}
+
+// Função para adicionar botão de avançar 10 segundos
+function addForwardButton(playerInstance) {
+  const playerContainer = playerInstance.getContainer();
+  const rewindContainer = playerContainer.querySelector(".jw-display-icon-rewind");
+  const forwardContainer = rewindContainer.cloneNode(true);
+  const forwardDisplayButton = forwardContainer.querySelector(".jw-icon-rewind");
+
+  forwardDisplayButton.style.transform = "scaleX(-1)";
+  forwardDisplayButton.ariaLabel = "Forward 10 Seconds";
+
+  const nextContainer = playerContainer.querySelector(".jw-display-icon-next");
+  nextContainer.parentNode.insertBefore(forwardContainer, nextContainer);
+
+  const buttonContainer = playerContainer.querySelector(".jw-button-container");
+  const rewindControlBarButton = buttonContainer.querySelector(".jw-icon-rewind");
+  const forwardControlBarButton = rewindControlBarButton.cloneNode(true);
+
+  forwardControlBarButton.style.transform = "scaleX(-1)";
+  forwardControlBarButton.ariaLabel = "Forward 10 Seconds";
+
+  rewindControlBarButton.parentNode.insertBefore(
+    forwardControlBarButton,
+    rewindControlBarButton.nextElementSibling
+  );
+
+  [forwardDisplayButton, forwardControlBarButton].forEach((button) => {
+    button.onclick = () => {
+      playerInstance.seek(playerInstance.getPosition() + 10);
+    };
+  });
+
+  // Oculta o botão "Next"
+  playerContainer.querySelector(".jw-display-icon-next").style.display = "none";
 }
