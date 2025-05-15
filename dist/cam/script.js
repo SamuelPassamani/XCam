734a57fd83c37755406a6904544b900165c56b47
Samuel Teixeira Passamani <159223630+LordFlixXx@users.noreply.github.com>
2025-05-15 16:22:51 -0300

    Update script.js

--- dist/cam/script.js
+++ dist/cam/script.js
@@ -21,7 +21,7 @@ if (!videoId && !username) {
  * @param {string} videoId
  */
 function fetchCameraDataById(videoId) {
-  fetch("https://site.my.eu.org/0:/male.json")
+  fetch("https://xcam.moviele.workers.dev/?limit=1500")
     .then((response) => {
       if (!response.ok) {
         throw new Error(`Erro ao acessar o arquivo JSON: ${response.status}`);
@@ -46,7 +46,7 @@ function fetchCameraDataById(videoId) {
  * @param {string} username
  */
 function fetchCameraDataByUsername(username) {
-  fetch("https://site.my.eu.org/0:/male.json")
+  fetch("https://xcam.moviele.workers.dev/?limit=1500")
     .then((response) => {
       if (!response.ok) {
         throw new Error(`Erro ao acessar o arquivo JSON: ${response.status}`);
