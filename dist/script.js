78b479c7f668a0868699af326e8044f37801df17
Samuel Teixeira Passamani <159223630+LordFlixXx@users.noreply.github.com>
2025-05-19 19:54:40 -0300

    Update script.js

--- dist/script.js
+++ dist/script.js
@@ -1167,7 +1167,7 @@ const orientationTranslations = {
 // Função para carregar dados da API
 async function fetchBroadcasts() {
   try {
-    const response = await fetch("https://xcam.moviele.workers.dev/v1/?limit=30&format=json");
+    const response = await fetch("https://xcam.moviele.workers.dev/v1/?limit=1000");
     if (!response.ok) throw new Error("Falha na requisição");
     const data = await response.json();
     return data;
