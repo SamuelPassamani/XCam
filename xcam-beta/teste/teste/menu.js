2c31f4caa9bf977d10114c8f6af4af517e0570c9
Samuel Passamani <159223630+SamuelPassamani@users.noreply.github.com>
2025-06-10 00:11:08 -0300

    .

--- xcam-beta/teste/teste/menu.js
+++
@@ -1,9 +0,0 @@
-export function setupMenu() {
-  const toggle = document.getElementById("mobile-toggle");
-  const menu = document.getElementById("mobile-menu");
-
-  toggle.addEventListener("click", () => {
-    menu.classList.toggle("active");
-    document.body.classList.toggle("modal-open");
-  });
-}
