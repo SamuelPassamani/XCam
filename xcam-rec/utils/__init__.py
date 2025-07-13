5b61a9f77c0e19033b3381ec2f4c29475455497e
Samuel Passamani <159223630+SamuelPassamani@users.noreply.github.com>
2025-07-13 04:10:28 -0300

    Create __init__.py

---
+++ xcam-rec/utils/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+# Este arquivo, mesmo que vazio, é necessário para que o Python
+# trate o diretório 'utils' como um pacote de módulos.
+# Isso nos permite fazer importações como: from utils.logger import log
